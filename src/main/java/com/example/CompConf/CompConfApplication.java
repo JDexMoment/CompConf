f12725e8b49847517943d97b009548177a7c8cf0
Evgeny Bychkov <jdexmoment@vk.com>
2024-04-14 12:30:40 +0300

    Initial commit

---
+++ src/main/java/com/example/CompConf/CompConfApplication.java
@@ -0,0 +1,13 @@
+package com.example.CompConf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompConfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompConfApplication.class, args);
+	}
+//sdfs
+}
